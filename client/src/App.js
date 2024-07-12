2849e7b68f06451607f34a8f81e9acee659a3693
Kye Clevenger <KyeClevenger@gmail.com>
2024-07-12 09:59:13 -0600

    courses

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
+import Courses from './components/Courses';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Landing />
+      <Landing/>
+      <Courses/>
     </div>
   );
 }
---
+++ client/src/components/Courses.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Courses = () => {
+    return (
+        <h1 className='bg-gray-400 h-56 text-center p-3'>Courses will be added here!</h1>
+    )
+}
+
+export default Courses
