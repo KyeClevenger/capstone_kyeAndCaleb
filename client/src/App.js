2ffca0c5ecdb543e0015fe2214cc07caeccfeae2
Kye Clevenger <KyeClevenger@gmail.com>
2024-07-12 09:09:33 -0600

    upToDate

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Landing />
     </div>
   );
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+return (
+    <div className='bg-gray-700 h-24'>
+    <h1 className='text-white text-center'>Navbar</h1>
+    </div>
+)
+}
+
+export default Navbar
