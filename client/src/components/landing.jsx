7a653bedc876e6b7b3fe31b6e04430d71e57b342
Kye Clevenger <KyeClevenger@gmail.com>
2024-07-12 08:56:10 -0600

    landingPage

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <h1>Landing</h1>
+    <div className='bg-gray-500 h-screen w-screen'>
+      <h1 className="text-7xl m-3 text-center">Landing Page</h1>
+    </div>
   );
 }
 
