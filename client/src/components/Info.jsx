3f1159acdf56e660e84745937e8430ae07b505fb
Kye Clevenger <KyeClevenger@gmail.com>
2024-07-17 19:46:04 -0600

    basicHomeDone

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -3,7 +3,7 @@ import StudentLifeImage from './pics/schoolLife.jpg';
 
 const Info = () => {
     return (
-        <div className='h-screen text-white bg-gray-300 p-12' id="info">
+        <div className='h-full text-white bg-gray-300 p-24' id="info">
             <h1 className='text-center text-md p-3 text-5xl underline underline-offset-8'>Student Life!</h1>
             <div className='flex items-center'>
             <img className=" rounded-full w-3/5 h-3/5 object-cover m-12 rounded-2xl" src={StudentLifeImage} alt="School Life" />
