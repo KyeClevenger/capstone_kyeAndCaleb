65b79265b22ef71f0effe2517de57ebe6b6b3820
Kye Clevenger <KyeClevenger@gmail.com>
2024-07-24 14:35:33 -0600

    mobileFixed

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -45,23 +45,22 @@ const Landing = () => {
   return (
     <div className='college h-screen' id="home">
       <div className='backdrop-blur-sm h-full text-white'>
-        <h1 className="text-7xl p-3 text-center pt-32">Calyek University</h1>
-        <h3 className='bg-gray-300 rounded rounded-xl font-semibold text-3xl text-gray-500 p-12 m-12 mt-24 text-justify w-3/5'>
+        <h1 className="text-5xl md:text-7xl p-3 text-center pt-24 md:pt-32">Calyek University</h1>
+        <h3 className='bg-gray-300 rounded rounded-xl font-semibold text-lg md:text-3xl text-gray-500 p-6 md:p-12 m-6 md:m-12 mt-12 md:mt-24 text-justify w-11/12 md:w-3/5 mx-auto'>
         Are you ready to take the next step in your educational journey? Calyek University is excited to welcome new and returning students for the upcoming Fall Semester. As you prepare for a new academic year, consider joining our vibrant and diverse community of learners and leaders.
         </h3>
         <div
           ref={scrollContainerRef}
-          className='scroll-container flex flex-nowrap gap-8 p-6 justify-center items-center overflow-x-auto'
-          style={{ display: 'flex', width: '100%' }}
+          className='scroll-container flex flex-nowrap gap-4 p-6 justify-center items-center overflow-x-auto w-full'
         >
           {[...images, ...images].map((image, index) => (
             <a
               key={index}
               href="#home"
               className="flex-shrink-0 border-4 border-gray-200 rounded-lg drop-shadow-lg"
-              style={{ minWidth: '200px' }}
+              style={{ minWidth: '150px', width: '350px' }}
             >
-              <img className='coverPics' src={image.src} alt={image.alt} />
+              <img className='coverPics w-full h-auto' src={image.src} alt={image.alt} />
             </a>
           ))}
         </div>
