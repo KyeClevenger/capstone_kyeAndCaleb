3e6f03927312788874878b29634b5e73cf8b7e76
Kye Clevenger <KyeClevenger@gmail.com>
2024-07-26 17:12:01 -0600

    profile

---
+++ client/src/components/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfilePage = () => {
+    return (
+        <div className='bg-gray-300 h-screen justify-center p-6 flex'>
+            <div className='bg-gray-800 w-3/5 rounded rouned-xl border border-4 border-white shadow-xl'>
+                <h1 className='text-5xl text-center p-12 text-white underline underline-offset-8'>Profile Page</h1>
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProfilePage
