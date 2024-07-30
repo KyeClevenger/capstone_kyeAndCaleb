6300313ca53c74c921d3131421d5cbd4691466e8
calebtd <caleb@dillenbeck.org>
2024-07-29 23:52:46 -0600

    Started backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Landing from './components/landing';
-import Info from './components/Info';
-import Courses from './components/Courses';
-import CoursesPage from './components/CoursesPage';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Landing from "./components/landing";
+import Info from "./components/Info";
+import Courses from "./components/Courses";
+import CoursesPage from "./components/CoursesPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CS_Class from './components/Classes/CS_Class';
-import Science_Class from './components/Classes/Science_Class';
-import English_Class from './components/Classes/English_Class';
-import Animation_Class from './components/Classes/Animation_Class';
-import Vet_Class from './components/Classes/Vet_Class';
-import Nursing_Class from './components/Classes/Nursing_Class';
-import Math_Class from './components/Classes/Math_Class';
-import Engineering_Class from './components/Classes/Engineering_Class';
-import ProfilePage from './components/ProfilePage';
+import CS_Class from "./components/Classes/CS_Class";
+import Science_Class from "./components/Classes/Science_Class";
+import English_Class from "./components/Classes/English_Class";
+import Animation_Class from "./components/Classes/Animation_Class";
+import Vet_Class from "./components/Classes/Vet_Class";
+import Nursing_Class from "./components/Classes/Nursing_Class";
+import Math_Class from "./components/Classes/Math_Class";
+import Engineering_Class from "./components/Classes/Engineering_Class";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={
-          <>
-            <Landing />
-            <Info />
-            <h1 className="text-center p-8 text-5xl font-semibold bg-gray-400 text-white underline underline-offset-8">Popular Courses</h1>
-            <Courses />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <Landing />
+              <Info />
+              <h1 className="text-center p-8 text-5xl font-semibold bg-gray-400 text-white underline underline-offset-8">
+                Popular Courses
+              </h1>
+              <Courses />
+            </>
+          }
+        />
         <Route path="/courses" element={<CoursesPage />} />
-        <Route path="cs_class" element={<CS_Class/>} />
-        <Route path="science_class" element={<Science_Class/>} />
-        <Route path="english_class" element={<English_Class/>} />
-        <Route path="animation_class" element={<Animation_Class/>} />
-        <Route path="veterinary_class" element={<Vet_Class/>} />
-        <Route path="nursing_class" element={<Nursing_Class/>} />
-        <Route path="math_class" element={<Math_Class/>} />
-        <Route path="engineering_class" element={<Engineering_Class/>} />
-        <Route path="profile" element={<ProfilePage/>} />
+        <Route path="cs_class" element={<CS_Class />} />
+        <Route path="science_class" element={<Science_Class />} />
+        <Route path="english_class" element={<English_Class />} />
+        <Route path="animation_class" element={<Animation_Class />} />
+        <Route path="veterinary_class" element={<Vet_Class />} />
+        <Route path="nursing_class" element={<Nursing_Class />} />
+        <Route path="math_class" element={<Math_Class />} />
+        <Route path="engineering_class" element={<Engineering_Class />} />
+        <Route path="profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
--- server/server.js
+++ server/server.js
@@ -1,31 +1,54 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./routes/users');
+// server/server.js
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/users");
 
 // Express app
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use (cors());
+app.use(cors());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+  console.log(req.path, req.method);
+  next();
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Failed to connect to MongoDB:', err));
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
+
+// Serve React files
+const path = require("path");
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
